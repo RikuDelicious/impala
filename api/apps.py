d49cfa5ff400c62e90e026dcf28c005bda58fc82
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-02 09:42:17 +0900

    add di container to api

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+from . import container
+
 
 class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api"
+
+    def ready(self):
+        container.wire(packages=["api"])
