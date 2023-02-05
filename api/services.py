ad26fece7780c95743801923dd50def0591024a2
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-05 21:12:22 +0900

    route_querydictメソッドを実装

--- api/services.py
+++ api/services.py
@@ -19,11 +19,31 @@ class ImageProcessingServiceAbstract(ABC):
 
 
 class ImageProcessingService(ImageProcessingServiceAbstract):
+    form_classes: list[ImageProfileForm] = []
+
     def route_querydict(self, querydict: QueryDict) -> ImageProfileForm:
         """
         引数querydictを解析できない場合、QueryError例外が発生する
         """
-        raise NotImplementedError()
+        profile_type = querydict.get("profile_type", None)
+
+        for form_class in self.form_classes:
+            if profile_type == form_class.get_profile_type():
+                form = form_class(querydict)
+                return form
+
+        if profile_type is None:
+            error_message = "このフィールドは必須です"
+        else:
+            expected_values = ", ".join(
+                [
+                    f'"{form_class.get_profile_type()}"'
+                    for form_class in self.form_classes
+                ]
+            )
+            error_message = f"このフィールドには次のうちいずれかの値を入力してください。({expected_values})"
+
+        raise QueryError({"profile_type": [error_message]})
 
     def create_profile(self, querydict: QueryDict) -> ImageProfileAbstract:
         profile_form = self.route_querydict(querydict)
