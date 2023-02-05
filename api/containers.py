cd1e9b5e8e7b5b0db1154c910898fba2a2b5adee
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-05 19:16:38 +0900

    コンテナにシングルトンのプロバイダを追加

--- api/containers.py
+++ api/containers.py
@@ -1,5 +1,8 @@
 from dependency_injector import containers, providers
 
+from .services import ImageModelService, ImageProcessingService
+
 
 class Container(containers.DeclarativeContainer):
-    ...
+    image_processing_service = providers.Singleton(ImageProcessingService)
+    image_model_service = providers.Singleton(ImageModelService)
