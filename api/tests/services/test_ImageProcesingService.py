40356728807a8b86ca07c661f37168a5855f93b8
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-05 22:03:18 +0900

    route_queryメソッドのテストを作成

--- api/tests/services/test_ImageProcesingService.py
+++ api/tests/services/test_ImageProcesingService.py
@@ -19,23 +19,38 @@ class ImageProfileFormStub(ImageProfileForm):
     intfield = forms.IntegerField()
 
     def get_profile(self) -> ImageProfileAbstract:
-        return ImageProfileFormStub()
+        return ImageProfileStub()
+
+    @classmethod
+    def get_profile_type(self) -> str:
+        return "jpeg_plain"
+
+
+class ImageProfileFormStub2(ImageProfileForm):
+    intfield = forms.IntegerField()
+
+    def get_profile(self) -> ImageProfileAbstract:
+        return ImageProfileStub()
+
+    @classmethod
+    def get_profile_type(self) -> str:
+        return "png_plain"
 
 
 # Tests
 ########################################################################################
 
 
-def test_create_file_query_valid():
+def test_create_profile_query_valid():
     service = ImageProcessingService()
     form = ImageProfileFormStub({"intfield": 1})
     querydict = QueryDict()
     with patch.object(service, "route_querydict", return_value=form):
         result = service.create_profile(querydict)
-        assert isinstance(result, ImageProfileFormStub)
+        assert isinstance(result, ImageProfileStub)
 
 
-def test_create_file_query_invalid():
+def test_create_profile_query_invalid():
     service = ImageProcessingService()
     form = ImageProfileFormStub({"intfield": "hoge"})
     querydict = QueryDict()
@@ -43,3 +58,35 @@ def test_create_file_query_invalid():
         with pytest.raises(QueryError) as exc_info:
             service.create_profile(querydict)
         assert exc_info.value.messages == dict(form.errors)
+
+
+def test_route_query_form_routed():
+    service = ImageProcessingService()
+    service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
+    querydict = QueryDict("a=1&profile_type=jpeg_plain&b=2")
+
+    result = service.route_querydict(querydict)
+    assert isinstance(result, ImageProfileFormStub)
+
+
+def test_route_query_form_not_routed():
+    service = ImageProcessingService()
+    service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
+    querydict = QueryDict("a=1&profile_type=not_supported_type&b=2")
+
+    with pytest.raises(QueryError) as exc_info:
+        service.route_querydict(querydict)
+    error_message = 'このフィールドには次のうちいずれかの値を入力してください。("png_plain", "jpeg_plain")'
+    assert exc_info.value.messages == {"profile_type": [error_message]}
+
+
+def test_route_query_None():
+    service = ImageProcessingService()
+    service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
+    querydict = QueryDict("a=1&b=2")
+
+    with pytest.raises(QueryError) as exc_info:
+        service.route_querydict(querydict)
+
+    error_message = "このフィールドは必須です"
+    assert exc_info.value.messages == {"profile_type": [error_message]}
