b65ac137dc67d0b39c7265b72418bb1e7244f391
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-08 20:57:05 +0900

    スタブ・コメント・関数名を少し修正

--- api/tests/services/test_ImageProcesingService.py
+++ api/tests/services/test_ImageProcesingService.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 from unittest.mock import patch
 
 import pytest
 from django import forms
 from django.http import QueryDict
+from PIL.Image import Image
 
 from api.image_processing import ImageProfileAbstract, ImageProfileForm, QueryError
 from api.services import ImageProcessingService
@@ -12,7 +15,20 @@ from api.services import ImageProcessingService
 
 
 class ImageProfileStub(ImageProfileAbstract):
-    pass
+    def create_pil_image(self) -> Image:
+        raise NotImplementedError()
+
+    @property
+    def quality(self) -> int | None:
+        raise NotImplementedError()
+
+    @quality.setter
+    def quality(self, value: int):
+        raise NotImplementedError()
+
+    @classmethod
+    def get_extension(cls) -> str:
+        raise NotImplementedError()
 
 
 class ImageProfileFormStub(ImageProfileForm):
@@ -40,6 +56,8 @@ class ImageProfileFormStub2(ImageProfileForm):
 # Tests
 ########################################################################################
 
+# ImageProcessingService.create_profile()
+
 
 def test_create_profile_query_valid():
     service = ImageProcessingService()
@@ -60,7 +78,10 @@ def test_create_profile_query_invalid():
         assert exc_info.value.messages == dict(form.errors)
 
 
-def test_route_query_form_routed():
+# ImageProcessingService.route_querydict()
+
+
+def test_route_querydict_form_routed():
     service = ImageProcessingService()
     service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
     querydict = QueryDict("a=1&profile_type=jpeg_plain&b=2")
@@ -69,7 +90,7 @@ def test_route_query_form_routed():
     assert isinstance(result, ImageProfileFormStub)
 
 
-def test_route_query_form_not_routed():
+def test_route_querydict_form_not_routed():
     service = ImageProcessingService()
     service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
     querydict = QueryDict("a=1&profile_type=not_supported_type&b=2")
@@ -80,7 +101,7 @@ def test_route_query_form_not_routed():
     assert exc_info.value.messages == {"profile_type": [error_message]}
 
 
-def test_route_query_None():
+def test_route_querydict_None():
     service = ImageProcessingService()
     service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
     querydict = QueryDict("a=1&b=2")
@@ -90,3 +111,6 @@ def test_route_query_None():
 
     error_message = "このフィールドは必須です"
     assert exc_info.value.messages == {"profile_type": [error_message]}
+
+
+# ImageProcessingService.create_image()
