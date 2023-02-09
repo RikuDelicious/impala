582f54563230f9d69275092bfd560448bd3fdf51
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-09 11:51:12 +0900

    クエリ文字列が正しくない場合のテストを追加

--- api/tests/image_processing/test_JPEGPlainProfileForm.py
+++ api/tests/image_processing/test_JPEGPlainProfileForm.py
@@ -1,7 +1,15 @@
 import pytest
 from django.http import QueryDict
 
-from api.image_processing import ColorRGB, JPEGPlainProfile, JPEGPlainProfileForm
+from api.image_processing import (
+    ColorRGB,
+    JPEGPlainProfile,
+    JPEGPlainProfileForm,
+    QueryError,
+)
+
+# Fixtures
+########################################################################################
 
 
 @pytest.fixture
@@ -9,6 +17,15 @@ def querydict_all_proper():
     return QueryDict(
         "profile_type=jpeg_plain&width=512&height=256&color_rgb=85CDFD&quality=50"
     )
+
+
+@pytest.fixture
+def querydict_improper():
+    return QueryDict("profile_type=jpeg_plain&width=512&height=256&quality=50")
+
+
+# Tests
+########################################################################################
 
 
 def test_get_profile_type():
@@ -35,3 +52,15 @@ def test_querydict_all_proper_get_profile(querydict_all_proper):
     assert profile.height == 256
     assert profile.color_rgb == ColorRGB(r=133, g=205, b=253)
     assert profile.quality == 50
+
+
+def test_querydict_improper_bind(querydict_improper):
+    form = JPEGPlainProfileForm(querydict_improper)
+    assert not form.is_valid()
+    assert bool(form.errors)
+
+
+def test_querydict_improper_get_profile(querydict_improper):
+    form = JPEGPlainProfileForm(querydict_improper)
+    with pytest.raises(QueryError):
+        form.get_profile()
