25a0ebff61ea22a6e87f4edbd76f2f5d235082a1
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-12 22:46:13 +0900

    サービスクラスのメソッドを全てクラスメソッドに変更

--- api/services.py
+++ api/services.py
@@ -18,12 +18,14 @@ from .image_processing import (
 
 
 class ImageProcessingServiceAbstract(ABC):
+    @classmethod
     @abstractmethod
-    def create_profile(self, querydict: QueryDict) -> ImageProfileAbstract:
+    def create_profile(cls, querydict: QueryDict) -> ImageProfileAbstract:
         raise NotImplementedError()
 
+    @classmethod
     @abstractmethod
-    def create_image(self, profile: ImageProfileAbstract, base_dir: str) -> str:
+    def create_image(cls, profile: ImageProfileAbstract, base_dir: str) -> str:
         raise NotImplementedError()
 
 
@@ -33,13 +35,14 @@ class ImageProcessingService(ImageProcessingServiceAbstract):
         PNGPlainProfileForm,
     ]
 
-    def route_querydict(self, querydict: QueryDict) -> ImageProfileForm:
+    @classmethod
+    def route_querydict(cls, querydict: QueryDict) -> ImageProfileForm:
         """
         引数querydictを解析できない場合、QueryError例外が発生する
         """
         profile_type = querydict.get("profile_type", None)
 
-        for form_class in self.form_classes:
+        for form_class in cls.form_classes:
             if profile_type == form_class.get_profile_type():
                 form = form_class(querydict)
                 return form
@@ -50,21 +53,23 @@ class ImageProcessingService(ImageProcessingServiceAbstract):
             expected_values = ", ".join(
                 [
                     f'"{form_class.get_profile_type()}"'
-                    for form_class in self.form_classes
+                    for form_class in cls.form_classes
                 ]
             )
             error_message = f"このフィールドには次のうちいずれかの値を入力してください。({expected_values})"
 
         raise QueryError({"profile_type": [error_message]})
 
-    def create_profile(self, querydict: QueryDict) -> ImageProfileAbstract:
-        profile_form = self.route_querydict(querydict)
+    @classmethod
+    def create_profile(cls, querydict: QueryDict) -> ImageProfileAbstract:
+        profile_form = cls.route_querydict(querydict)
         if profile_form.is_valid():
             return profile_form.get_profile()
         else:
             raise QueryError(dict(profile_form.errors))
 
-    def create_image(self, profile: ImageProfileAbstract, base_dir: str) -> str:
+    @classmethod
+    def create_image(cls, profile: ImageProfileAbstract, base_dir: str) -> str:
         pil_image = profile.create_pil_image()
         if os.path.isdir(base_dir):
             tmp_image_path = os.path.join(base_dir, f"tmp.{profile.get_extension()}")
@@ -80,29 +85,33 @@ class ImageProcessingService(ImageProcessingServiceAbstract):
 
 
 class ImageModelServiceAbstract(ABC):
+    @classmethod
     @abstractmethod
-    def get_cache_image_url(self, profile: ImageProfileAbstract) -> str | None:
+    def get_cache_image_url(cls, profile: ImageProfileAbstract) -> str | None:
         raise NotImplementedError()
 
+    @classmethod
     @abstractmethod
-    def upload_image(self, image_path: str, profile: ImageProfileAbstract) -> str:
+    def upload_image(cls, image_path: str, profile: ImageProfileAbstract) -> str:
         raise NotImplementedError()
 
 
 class ImageModelService:
     model = models.Image
 
-    def get_cache_image_url(self, profile: ImageProfileAbstract) -> str | None:
-        queryset = self.model.objects.filter(profile_signiture=profile.dump_signiture())
+    @classmethod
+    def get_cache_image_url(cls, profile: ImageProfileAbstract) -> str | None:
+        queryset = cls.model.objects.filter(profile_signiture=profile.dump_signiture())
         if queryset.exists():
             return queryset.first()
         else:
             return None
 
-    def upload_image(self, image_path: str, profile: ImageProfileAbstract) -> str:
+    @classmethod
+    def upload_image(cls, image_path: str, profile: ImageProfileAbstract) -> str:
         with open(image_path, "rb") as f:
             upload_file = File(f, name=profile.upload_file_name)
-            image = self.model.objects.create(
+            image = cls.model.objects.create(
                 upload=upload_file, profile_signiture=profile.dump_signiture()
             )
             return image.upload.url
