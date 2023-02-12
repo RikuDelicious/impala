45c84a64e8d94e5593c8d651e6d7abffcec0faac
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-12 19:13:52 +0900

    Imageモデル追加

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Image(models.Model):
+    upload = models.ImageField(upload_to="images/", max_length=1024)
+    profile_signiture = models.CharField(max_length=255, unique=True)
