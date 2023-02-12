868c7883259a4fa09e77ab48431a47ff6acdfa61
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-12 23:04:28 +0900

    テスト関数内のテスト対象をクラスメソッドに変更

--- api/tests/services/test_ImageProcessingService.py
+++ api/tests/services/test_ImageProcessingService.py
@@ -27,9 +27,16 @@ class ImageProfileStub(ImageProfileAbstract):
     def quality(self) -> int | None:
         return self._quality
 
+    @property
+    def upload_file_name(self) -> str:
+        raise NotImplementedError()
+
     @classmethod
     def get_extension(cls) -> str:
         return "jpeg"
+
+    def dump_signiture(self) -> str:
+        raise NotImplementedError()
 
 
 class ImageProfileFormStub(ImageProfileForm):
@@ -71,21 +78,19 @@ def temp_dir():
 
 
 def test_create_profile_query_valid():
-    service = ImageProcessingService()
     form = ImageProfileFormStub({"intfield": 1})
     querydict = QueryDict()
-    with patch.object(service, "route_querydict", return_value=form):
-        result = service.create_profile(querydict)
+    with patch.object(ImageProcessingService, "route_querydict", return_value=form):
+        result = ImageProcessingService.create_profile(querydict)
         assert isinstance(result, ImageProfileStub)
 
 
 def test_create_profile_query_invalid():
-    service = ImageProcessingService()
     form = ImageProfileFormStub({"intfield": "hoge"})
     querydict = QueryDict()
-    with patch.object(service, "route_querydict", return_value=form):
+    with patch.object(ImageProcessingService, "route_querydict", return_value=form):
         with pytest.raises(QueryError) as exc_info:
-            service.create_profile(querydict)
+            ImageProcessingService.create_profile(querydict)
         assert exc_info.value.messages == dict(form.errors)
 
 
@@ -93,32 +98,29 @@ def test_create_profile_query_invalid():
 
 
 def test_route_querydict_form_routed():
-    service = ImageProcessingService()
-    service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
+    ImageProcessingService.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
     querydict = QueryDict("a=1&profile_type=jpeg_plain&b=2")
 
-    result = service.route_querydict(querydict)
+    result = ImageProcessingService.route_querydict(querydict)
     assert isinstance(result, ImageProfileFormStub)
 
 
 def test_route_querydict_form_not_routed():
-    service = ImageProcessingService()
-    service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
+    ImageProcessingService.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
     querydict = QueryDict("a=1&profile_type=not_supported_type&b=2")
 
     with pytest.raises(QueryError) as exc_info:
-        service.route_querydict(querydict)
+        ImageProcessingService.route_querydict(querydict)
     error_message = 'このフィールドには次のうちいずれかの値を入力してください。("png_plain", "jpeg_plain")'
     assert exc_info.value.messages == {"profile_type": [error_message]}
 
 
 def test_route_querydict_None():
-    service = ImageProcessingService()
-    service.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
+    ImageProcessingService.form_classes = [ImageProfileFormStub2, ImageProfileFormStub]
     querydict = QueryDict("a=1&b=2")
 
     with pytest.raises(QueryError) as exc_info:
-        service.route_querydict(querydict)
+        ImageProcessingService.route_querydict(querydict)
 
     error_message = "このフィールドは必須です"
     assert exc_info.value.messages == {"profile_type": [error_message]}
@@ -128,22 +130,23 @@ def test_route_querydict_None():
 
 
 def test_create_image_quality_None(temp_dir):
-    service = ImageProcessingService()
-    result_image_path = service.create_image(ImageProfileStub(quality=None), temp_dir)
+    result_image_path = ImageProcessingService.create_image(
+        ImageProfileStub(quality=None), temp_dir
+    )
     assert os.path.isfile(result_image_path)
     assert os.path.splitext(result_image_path)[1] == ".jpeg"
 
 
 def test_create_image_quality_75(temp_dir):
-    service = ImageProcessingService()
-    result_image_path = service.create_image(ImageProfileStub(quality=75), temp_dir)
+    result_image_path = ImageProcessingService.create_image(
+        ImageProfileStub(quality=75), temp_dir
+    )
     assert os.path.isfile(result_image_path)
     assert os.path.splitext(result_image_path)[1] == ".jpeg"
 
 
 def test_create_image_base_dir_not_exist():
-    service = ImageProcessingService()
     fake_dir = "/path/to/fake/dir"
     expected_error_message = "No such directory. base_dir: /path/to/fake/dir"
     with pytest.raises(FileNotFoundError, match=expected_error_message):
-        service.create_image(ImageProfileStub(quality=75), fake_dir)
+        ImageProcessingService.create_image(ImageProfileStub(quality=75), fake_dir)
