6171d022694f1fd30ff8a9ca6df7729b7594a62c
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-16 16:49:52 +0900

    postcssの設定及びプラグインの追加を行った

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// npx tailwindcss -i ./tailwind-input.css -o ./static/front/dist/style.css --watch
 module.exports = {
   content: [
     "./templates/front/**/*.html"
@@ -7,5 +6,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
